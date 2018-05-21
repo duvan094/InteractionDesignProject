58c218ef0b0711faa488cd12e04c8832c423f52b
Jacob <jacobduvander@gmail.com>
2018-05-21 13:30:57 +0200

    Hamburger animation

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ console.log("this is our main js file.");
 
 function toggleMenu(){
   document.getElementById("hamburgerBtn").classList.toggle("clicked");
+  document.querySelector("nav").classList.toggle("clicked");
 }

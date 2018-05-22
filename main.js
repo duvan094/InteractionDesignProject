0a7b05f8826c923fe1517c01fae560c0d835e3e9
Jacob <jacobduvander@gmail.com>
2018-05-22 19:12:47 +0200

    Added cooler slide animation etc
    Added cooler slide animation and upside down toggle arrow for hamburger
    menu.

--- main.js
+++ main.js
@@ -71,19 +71,18 @@ function expandMenu(event){
 function toggleList(event){
   event.preventDefault();
   var parent = event.target.parentNode;
-  var list = parent.children[parent.children.length-1];
 
   var previouslyExpanded = document.querySelectorAll(".expand");
 
   if(previouslyExpanded.length !== 0){//Check if there are any previously expanded elements
     previouslyExpanded[0].classList.remove("expand");
-    previouslyExpanded[0].style.height = 0 + "px";
-    if(previouslyExpanded[0] !== list){
-      list.classList.add("expand");
-      list.style.height = list.children.length * parent.children[0].offsetHeight + "px";
+    previouslyExpanded[0].children[1].style.height = 0 + "px";
+    if(previouslyExpanded[0] !== parent){
+      parent.classList.add("expand");
+      parent.children[1].style.height = parent.children[1].children.length * parent.children[0].offsetHeight + "px";
     }
   }else{
-    list.classList.add("expand");
-    list.style.height = list.children.length * parent.children[0].offsetHeight + "px";
+    parent.classList.add("expand");
+    parent.children[1].style.height = parent.children[1].children.length * parent.children[0].offsetHeight + "px";
   }
 }

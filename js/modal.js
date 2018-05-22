3e44dce19b47c89f8df146564d38010d40f54e84
Swindian95 <mihir.chouhan1@gmail.com>
2018-05-22 16:36:53 +0200

    modal JS

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,31 @@
-var modal = document.getElementById("modal");
+var outerSlider = document.getElementById("outerSlider");
+console.log(outerSlider);
+
+var signForm = document.getElementById("signForm");
+
+var cardContainer = document.getElementById("cardContainer");
+
 
 function closeModal(){
-  modal.classList.remove("transition");
+  modal.classList.remove("display");
 
-  setTimeout(function(){
-    modal.classList.remove("display");
-  },300);//300milliseconds
+};
+
+function nextScreen(container,index){
+  var outerSlider = document.getElementById(container);
+  var length = outerSlider.children.length;
+  outerSlider.style.transform = "translate(" + (index/length)*-100 + "%, 0)";
+  console.log((index/length)*-100);
+  console.log("hi");
+};
+
+
+function nextCard(){
+  signForm.style.transform = "translate(-20%, 0)";
+  console.log("suckadic");
+};
+
+function nextOne(){
+  cardContainer.style.transform = "translate(-20%, 0)";
+  console.log("please");
+};

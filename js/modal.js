ecaf9d25722c8f841ce4f05f9e34dae79b7362b8
Swindian95 <mihir.chouhan1@gmail.com>
2018-05-23 13:22:12 +0200

    Modals now kinda works

--- js/modal.js
+++ js/modal.js
@@ -11,21 +11,38 @@ function closeModal(){
 
 };
 
+function validateForm(event){
+  event.preventDefault();
+
+  var valid = true;
+
+  if(valid){
+    nextScreen('signUpCont',1);
+  }else{
+    console.log("not valid, go home!")
+  }
+}
+
+function submitEmailPw(event){
+  event.preventDefault();
+
+  if(validEmail() && validPassword()){
+    nextScreen('signUpCont',2);
+  }else{
+    console.log("Write a valid email and password dammit!");
+  }
+}
+
+function validEmail(){
+  return true;
+}
+
+function validPassword(){
+  return true;
+}
+
 function nextScreen(container,index){
   var outerSlider = document.getElementById(container);
   var length = outerSlider.children.length;
   outerSlider.style.transform = "translate(" + (index/length)*-100 + "%, 0)";
-  console.log((index/length)*-100);
-  console.log("hi");
-};
-
-
-function nextCard(){
-  signForm.style.transform = "translate(-20%, 0)";
-  console.log("suckadic");
-};
-
-function nextOne(){
-  cardContainer.style.transform = "translate(-20%, 0)";
-  console.log("please");
 };

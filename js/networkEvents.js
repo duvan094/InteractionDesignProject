073994a062ce80a5c06fa65f8d90ccefc7927dcc
Jacob <jacobduvander@gmail.com>
2018-05-26 18:52:14 +0200

    Removed "fuck y'all" console.logs

--- js/networkEvents.js
+++ js/networkEvents.js
@@ -10,8 +10,8 @@ resizeSlider();
 function resizeSlider(){
   var element = document.querySelector(".eventList");
   var width = element.offsetWidth;
-  console.log(width);
-  for(var i = 0; eventSliders.length; i++){
+
+  for(var i = 0; i < eventSliders.length; i++){
 
       for(var x = 0; x < eventSliders[i].children.length; x++){
         eventSliders[i].children[x].style.width = width + "px";
@@ -36,12 +36,10 @@ function eventConf(container,index){
   var slideEvent = document.getElementById(container);
   var length = slideEvent.children.length;
   slideEvent.style.transform = "translate(" + (index/length)*-100 + "%, 0)";
-  console.log("hehe");
 }
 
 function closeEvent(event){
   event.preventDefault();
   event.target.parentNode.parentNode.parentNode.children[0].classList.remove("clicked");
   event.target.parentNode.parentNode.parentNode.children[1].classList.remove("clicked");
-  console.log("fuckallyall");
 }
